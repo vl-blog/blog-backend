b089741b1223fb27745d3a69f6f65e3694f1cb9d
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-02-28 20:20:48 +0200

    Create build configurations for TeamCity and GitHub Actions

--- build/Build.cs
+++ build/Build.cs
@@ -1,17 +1,42 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.CI.TeamCity;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
+[TeamCity(
+    TeamCityAgentPlatform.Unix,
+    Version = "2020.2",
+    VcsTriggeredTargets = new[] {nameof(Test)},
+    NonEntryTargets = new[] {nameof(Restore)},
+    ExcludedTargets = new[] {nameof(Clean)})]
+[GitHubActions(
+    "build",
+    GitHubActionsImage.WindowsLatest,
+    GitHubActionsImage.UbuntuLatest,
+    GitHubActionsImage.MacOsLatest,
+    OnPushBranches = new[] {"master"},
+    InvokedTargets = new[] {nameof(Compile)})]
+[GitHubActions(
+    "test",
+    GitHubActionsImage.WindowsLatest,
+    GitHubActionsImage.UbuntuLatest,
+    GitHubActionsImage.MacOsLatest,
+    OnPushBranches = new[] {"master"},
+    InvokedTargets = new[] {nameof(Test)})]
 class Build : NukeBuild
 {
     public static int Main () => Execute<Build>(x => x.Compile);
@@ -20,22 +45,54 @@ class Build : NukeBuild
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     [Solution] readonly Solution Solution;
+    
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath TestsDirectory => RootDirectory / "tests";
+    AbsolutePath OutputDirectory => RootDirectory / "output";
 
     Target Clean => _ => _
-        .Before(Restore)
         .Executes(() =>
         {
+            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
         });
 
     Target Restore => _ => _
+        .DependsOn(Clean)
         .Executes(() =>
         {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
         });
 
+    [Partition(2)] readonly Partition TestPartition;
+    IEnumerable<Project> TestProjects => TestPartition.GetCurrent(Solution.GetProjects("*Tests"));
+    AbsolutePath TestResultDirectory => OutputDirectory / "test-results";
+    
+    Target Test => _ => _
+        .Partition(() => TestPartition)
+        .DependsOn(Compile)
+        .Produces(TestResultDirectory / "*.trx")
+        .Executes(() =>
+        {
+            DotNetTest(_ => _
+                    .SetConfiguration(Configuration)
+                    .SetVerbosity(DotNetVerbosity.Minimal)
+                    .SetNoBuild(InvokedTargets.Contains(Compile))
+                    .SetResultsDirectory(TestResultDirectory)
+                    .CombineWith(TestProjects, (_, v) => _
+                        .SetProjectFile(v)
+                        .SetLogger($"trx;LogFileName={v.Name}.trx")),
+                completeOnFailure: false);
+        });
 }
