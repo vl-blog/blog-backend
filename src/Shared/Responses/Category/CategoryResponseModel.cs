4318ef3b6e1ff7d56049dd81f9e387681889c042
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-03-07 15:00:04 +0200

    Add category block component and its subcomponents

---
+++ src/Shared/Responses/Category/CategoryResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using VovaLantsovBlog.Shared.Responses.Post;
+
+namespace VovaLantsovBlog.Shared.Responses.Category
+{
+    public sealed record CategoryResponseModel
+    {
+        public string CategoryName { get; init; }
+        public string CategoryId { get; init; }
+        public List<PostPreviewResponseModel> Posts { get; init; }
+    }
+}
---
+++ src/Shared/Responses/Post/PostPreviewResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace VovaLantsovBlog.Shared.Responses.Post
+{
+    public sealed record PostPreviewResponseModel
+    {
+        public string PostId { get; init; }
+        public string PostTitle { get; init; }
+        public string LastEditedTime { get; init; }
+        public string ImageUrl { get; init; }
+    }
+}
