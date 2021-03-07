9d781e3031039dd087cbf4f9d89a9b18b434800c
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-03-07 17:48:05 +0200

    Fix http client requests

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,7 +17,7 @@ namespace VovaLantsovBlog.Client
                 client.BaseAddress = new Uri("https://blog.vova-lantsov.dev/"));
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
-            //builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("VovaLantsovBlog.ServerAPI"));
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("VovaLantsovBlog.ServerAPI"));
 
             await builder.Build().RunAsync();
         }
--- src/Shared/Responses/Category/CategoryResponseModel.cs
+++ src/Shared/Responses/Category/CategoryResponseModel.cs
@@ -7,6 +7,6 @@ namespace VovaLantsovBlog.Shared.Responses.Category
     {
         public string CategoryName { get; init; }
         public string CategoryId { get; init; }
-        public List<PostPreviewResponseModel> Posts { get; init; }
+        public List<PostPreviewResponseModel>? Posts { get; init; }
     }
 }
