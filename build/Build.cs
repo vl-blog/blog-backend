95e5abf6e1ba6d3241f8a5377d67818d1c2ed976
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-10-23 15:48:29 +0300

    Update Nuke to 5.3.0; Update projects to target .NET 6.0.0-rc.2

--- build/Build.cs
+++ build/Build.cs
@@ -1,35 +1,17 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
-using Nuke.Common.CI.TeamCity;
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
-[TeamCity(
-    TeamCityAgentPlatform.Unix,
-    Version = "2020.2",
-    VcsTriggeredTargets = new[] {nameof(Test), nameof(Up)},
-    NonEntryTargets = new[] {nameof(Restore)},
-    ExcludedTargets = new[] {nameof(Clean)})]
-[GitHubActions(
-    "build",
-    GitHubActionsImage.WindowsLatest,
-    GitHubActionsImage.UbuntuLatest,
-    GitHubActionsImage.MacOsLatest,
-    OnPushBranches = new[] {"master"},
-    InvokedTargets = new[] {nameof(Compile)})]
 [GitHubActions(
     "test",
     GitHubActionsImage.WindowsLatest,
@@ -37,42 +19,18 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
     GitHubActionsImage.MacOsLatest,
     OnPushBranches = new[] {"master"},
     InvokedTargets = new[] {nameof(Test)})]
-class Build : NukeBuild
+partial class Build : NukeBuild
 {
-    public static int Main () => Execute<Build>(x => x.Up);
+    public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
-    
-    [Parameter("Check to wipe the database data on shutdown")]
-    readonly bool WipeDatabaseData;
-
-    [Parameter("The name of docker-compose project")]
-    readonly string ProjectName = "blog";
 
     [Solution] readonly Solution Solution;
     
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
     AbsolutePath OutputDirectory => RootDirectory / "output";
-    
-    [PathExecutable("docker-compose")] readonly Tool DockerCompose;
-    
-    Target Down => _ => _
-        .Executes(() =>
-        {
-            string command = $"-p {ProjectName} down";
-            if (WipeDatabaseData)
-                command += " --volumes";
-            DockerCompose(command, SourceDirectory);
-        });
-
-    Target Up => _ => _
-        .DependsOn(Down)
-        .Executes(() =>
-        {
-            DockerCompose($"-p {ProjectName} up --build -d --remove-orphans", SourceDirectory);
-        });
 
     Target Clean => _ => _
         .Executes(() =>
@@ -104,7 +62,7 @@ class Build : NukeBuild
     AbsolutePath TestResultDirectory => OutputDirectory / "test-results";
     
     Target Test => _ => _
-        .Partition(() => TestPartition)
+        .Partition(2)
         .DependsOn(Compile)
         .Produces(TestResultDirectory / "*.trx")
         .Executes(() =>
@@ -116,7 +74,7 @@ class Build : NukeBuild
                     .SetResultsDirectory(TestResultDirectory)
                     .CombineWith(TestProjects, (_, v) => _
                         .SetProjectFile(v)
-                        .SetLogger($"trx;LogFileName={v.Name}.trx")),
+                        .SetLoggers($"trx;LogFileName={v.Name}.trx")),
                 completeOnFailure: false);
         });
 }
