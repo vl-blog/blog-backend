447225d6078341a73d5e81244a67c8322f4ee64c
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2022-01-31 01:25:06 +0200

    Use new WebApplication

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,56 +1,88 @@
-﻿using System;
-using System.Linq;
-using Microsoft.AspNetCore.Hosting;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using VovaLantsovBlog.Data;
 using VovaLantsovBlog.Server.Authentication;
+using VovaLantsovBlog.Shared;
 
-namespace VovaLantsovBlog.Server;
+var builder = WebApplication.CreateBuilder(args);
 
-public static class Program
+string env = builder.Environment.EnvironmentName;
+builder.Configuration
+	.AddJsonFile("dbsettings.json", false, false)
+	.AddJsonFile($"dbsettings.{env}.json", true, false)
+	.AddJsonFile("sentrysettings.json", false, false)
+	.AddJsonFile($"sentrysettings.{env}.json", true, false)
+	.AddJsonFile("jwt.json", false, false)
+	.AddJsonFile($"jwt.{env}.json", true, false);
+
+builder.Logging.AddConfiguration(builder.Configuration);
+builder.Logging.AddDebug();
+builder.Logging.AddConsole();
+
+builder.WebHost.UseSentry();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDbContext<BlogContext>(options =>
+	options.UseNpgsql(builder.Configuration.GetConnectionString("BlogConnectionString"), npgsqlBuilder =>
+		npgsqlBuilder.EnableRetryOnFailure()
+			.MigrationsAssembly("VovaLantsovBlog.Data")
+			.MigrationsHistoryTable("__MigrationHistory", Constants.SchemaName)));
+builder.Services.AddDbContext<AuthDbContext>(options =>
+	options.UseNpgsql(builder.Configuration.GetConnectionString("BlogConnectionString"), npgsqlBuilder =>
+		npgsqlBuilder.EnableRetryOnFailure()
+			.MigrationsAssembly("VovaLantsovBlog.Server")
+			.MigrationsHistoryTable("__AuthMigrationHistory", Constants.SchemaName)));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+	.AddRoles<IdentityRole>()
+	.AddRoleManager<RoleManager<IdentityRole>>()
+	.AddEntityFrameworkStores<AuthDbContext>();
+
+builder.Services.AddAuthentication(options =>
+	{
+		options.DefaultScheme = options.DefaultAuthenticateScheme =
+			options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+	})
+	.AddJwtBearer(jwt =>
+	{
+		var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:SecretKey"]);
+
+		jwt.SaveToken = true;
+		jwt.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(key),
+			ValidateIssuer = false,
+			ValidateAudience = false,
+			RequireExpirationTime = false,
+			ValidateLifetime = true
+		};
+	});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var host = CreateHostBuilder(args).Build();
-        using (IServiceScope scope = host.Services.CreateScope())
-        {
-            IServiceProvider provider = scope.ServiceProvider;
-            BlogContext context = provider.GetRequiredService<BlogContext>();
-            AuthDbContext authContext = provider.GetRequiredService<AuthDbContext>();
-
-            if (context.Database.GetPendingMigrations().Any())
-                context.Database.Migrate();
-            if (authContext.Database.GetPendingMigrations().Any())
-                authContext.Database.Migrate();
-        }
-        host.Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((context, builder) =>
-            {
-                string env = context.HostingEnvironment.EnvironmentName;
-                builder.AddJsonFile("dbsettings.json", false, false)
-                    .AddJsonFile($"dbsettings.{env}.json", true, false)
-                    .AddJsonFile("sentrysettings.json", false, false)
-                    .AddJsonFile($"sentrysettings.{env}.json", true, false)
-                    .AddJsonFile("jwt.json", false, false)
-                    .AddJsonFile($"jwt.{env}.json", true, false);
-            })
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.ConfigureLogging((context, builder) =>
-                {
-                    builder.AddConfiguration(context.Configuration);
-                    builder.AddDebug();
-                    builder.AddConsole();
-                });
-                webBuilder.UseSentry();
-                webBuilder.UseStartup<Startup>();
-            });
+	app.UseDeveloperExceptionPage();
+	app.UseWebAssemblyDebugging();
 }
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapRazorPages();
+	endpoints.MapControllers();
+	endpoints.MapFallbackToFile("index.html");
+});
